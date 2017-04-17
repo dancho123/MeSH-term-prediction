f7ec853a9adec493a5a5380ba7cee0e3592fb7ad
Florian Beutler <florian.beutler@port.ac.uk>
2017-04-17 14:09:46 +0100

    Add files via upload

---
+++ process_pubmed_indep.py
@@ -0,0 +1,136 @@
+import os, sys
+import re, time
+import numpy as np
+from collections import Counter, defaultdict
+import cPickle
+
+from sklearn.preprocessing import LabelEncoder, MultiLabelBinarizer
+from sklearn.metrics import classification_report
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cross_validation import train_test_split
+
+import matplotlib.pyplot as plt
+
+'''
+Here we read in data extracted from the pubmed database and train machine
+learning models to predict the MeSH headings.
+'''
+
+def main():
+
+    path = '/tmp/'
+
+    print('Read data'),
+    time0 = time.time()
+    y, list_of_authors, list_of_titles, list_of_abstracts = read_data(path)
+    print 'done after %d seconds' % (time.time() - time0)
+
+    print('prepare features'),
+    # produce a (samples, feature) matrix
+    time0 = time.time()
+    X = prepare_data(list_of_authors, list_of_titles, list_of_abstracts)
+    print 'done after %d seconds' % (time.time() - time0)
+
+    print('transform class labels to numerals'),
+    time0 = time.time()
+    mlb = MultiLabelBinarizer()
+    y_numeric = mlb.fit_transform(y) 
+    print 'done after %d seconds' % (time.time() - time0)
+
+    print('Split data'),
+    time0 = time.time()
+    X_train, X_test, y_train, y_test = train_test_split(X, y_numeric, test_size=0.25, random_state=42)
+    print 'done after %d seconds' % (time.time() - time0)
+
+    '''
+    The OneVsRestClassifier strategy involves training a single classifier per class, with the samples of that class 
+    as positive samples and all other samples as negatives. 
+    Note that the OneVsRestClassifier does not take into account dependencies between MeSH terms, meaning if an input 
+    had two MeSH, it is equal to having two identical inputs with one MeSH term each. Relations between MeSH terms should 
+    help the classification, for example papers with MeSH term 'cancer' might have a higher probability to also have a 
+    MeSH term 'cancer treatment'. This extra information is currently not used.
+    '''
+
+    # Test different classifiers 
+    print "test Logistic Regression"
+    from sklearn.linear_model import LogisticRegression
+    clf = OneVsRestClassifier(LogisticRegression())
+    test_clf(clf, X_train, y_train, X_test, y_test, mlb)
+    print "done"
+
+    print "test MultinomialNB"
+    from sklearn.naive_bayes import MultinomialNB
+    clf = OneVsRestClassifier(MultinomialNB())
+    test_clf(clf, X_train, y_train, X_test, y_test, mlb)
+    print "done"
+
+    print "test LinearSVC"
+    from sklearn import svm
+    clf = OneVsRestClassifier(svm.LinearSVC(random_state=0))
+    test_clf(clf, X_train, y_train, X_test, y_test, mlb)
+    print "done"
+
+    print "test DecisionTreeRegressor"
+    from sklearn.tree import DecisionTreeRegressor
+    clf = OneVsRestClassifier(DecisionTreeRegressor(random_state=0))
+    test_clf(clf, X_train, y_train, X_test, y_test, mlb)
+    print "done"
+
+    return 
+
+
+def test_clf(clf, X_train, y_train, X_test, y_test, mlb):
+    print('Fit data'),
+    time0 = time.time()
+    clf.fit(X_train, y_train)
+    print 'done after %d seconds' % (time.time() - time0)
+
+    print('Predict data'),
+    time0 = time.time()
+    y_pred = clf.predict(X_test)
+    print 'done after %d seconds' % (time.time() - time0)
+    #for i, pred in enumerate(y_pred):
+    #    print pred
+    #    print y_test[i]
+    #    print "\n"
+
+    print('Evaluate data'),
+    time0 = time.time()
+    evaluate = [1 if (prediction == y_test[i]).all() else 0 for i, prediction in enumerate(y_pred)]
+    report = classification_report(y_test, y_pred, target_names=mlb.classes_)
+    print 'done after %d seconds' % (time.time() - time0)
+    print "prediction for %d papers" % len(y_pred)
+    print "fraction of correct prediction for all MeSH terms (including multiple MeSH terms) = ", float(sum(evaluate))/len(y_pred)
+    print report
+    return
+
+
+def read_data(path):
+    list_of_authors = cPickle.load(open('%sauthors.pickle' % path, 'rb'))
+    list_of_titles = cPickle.load(open('%stitles.pickle' % path, 'rb'))
+    list_of_abstracts = cPickle.load(open('%sabstracts.pickle' % path, 'rb'))
+    y = cPickle.load(open('%sy.pickle' % path, 'rb'))
+    return y, list_of_authors, list_of_titles, list_of_abstracts
+
+
+def prepare_data(list_of_authors, list_of_titles, list_of_abstracts):
+    # We calculate the tf-idf for each feature
+    vectorizer = TfidfVectorizer(analyzer = "word", tokenizer = None, preprocessor = None, stop_words = "english", max_features = 5000) 
+    train_data_features1 = vectorizer.fit_transform(list_of_abstracts)
+    train_data_features2 = vectorizer.fit_transform(list_of_titles)
+    train_data_features3 = vectorizer.transform(list_of_authors)
+
+    # Numpy arrays are easy to work with, so convert the results to arrays
+    train_data_features1 = train_data_features1.toarray()
+    train_data_features2 = train_data_features2.toarray()
+    train_data_features3 = train_data_features3.toarray()
+
+    X = np.c_[train_data_features1, train_data_features2, train_data_features3]
+    print "X = ", X.shape
+
+    return X
+
+
+if __name__ == "__main__":
+    main()
